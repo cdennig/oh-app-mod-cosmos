b82cc801a6b8cbe1c8cc22ff35089d247157e98f
johantysklind <jotyskl@microsoft.com>
2021-10-28 14:36:20 +0200

    Mat View Function
    Adding azure function

---
+++ afn-cosmos-mat2/afn-cosmos-mat2/topsales.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace afn_cosmos_mat2
+{
+    class TopSales
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+
+        public TopSales()
+        {
+            Id = Guid.NewGuid().ToString();
+        }
+
+    }
+}
